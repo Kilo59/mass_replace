71768613d85dffb67f73abf637337574b6e7a125
Gabriel <gabriel59kg@gmail.com>
2018-04-24 19:29:39 -0400

    use unix paths

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -77,16 +77,16 @@ def test_simple_file_find_and_replace():
 def test_basic_mass_replace():
     config_dict = {'filetypes': ['txt', 'csv'],
                    'replacement_pairs': {'a': 'z'}}
-    mr.mass_replace('tests/docs', config=config_dict)
-    txt_1st_str = read_file_lines('tests/docs/abc.txt')[0][0]
-    csv_1st_str = read_file_lines('tests/docs/abc.csv')[0][0]
+    mr.mass_replace('tests\\docs', config=config_dict)
+    txt_1st_str = read_file_lines('tests\\docs\\abc.txt')[0][0]
+    csv_1st_str = read_file_lines('tests\\docs\\abc.csv')[0][0]
     assert txt_1st_str == config_dict['replacement_pairs']['a']
     assert csv_1st_str == config_dict['replacement_pairs']['a']
     # reset files
     config_dict['replacement_pairs'] = {'z': 'a'}
-    mr.mass_replace('tests/docs', config=config_dict)
-    txt_1st_str = read_file_lines('tests/docs/abc.txt')[0][0]
-    csv_1st_str = read_file_lines('tests/docs/abc.csv')[0][0]
+    mr.mass_replace('tests\\docs', config=config_dict)
+    txt_1st_str = read_file_lines('tests\\docs\\abc.txt')[0][0]
+    csv_1st_str = read_file_lines('tests\\docs\\abc.csv')[0][0]
     assert txt_1st_str == config_dict['replacement_pairs']['z']
     assert csv_1st_str == config_dict['replacement_pairs']['z']
 
